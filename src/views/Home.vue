01f40e82c7e0afc912cbf7e380ddf7405529d9d1
skadiD <kazdel@qq.com>
2022-06-18 19:29:41 +0800

    GetLog

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="divider text-xl text-info">运行状态</div>
+  <div class="divider text-xl text-info">运行日志</div>
   <div class="flex flex-wrap flex-row justify-between">
-
+  <Console :data="logs" :next="next" />
   </div>
 </template>
 <script setup>
   import {ref} from "vue";
-  import Info from "../components/Card/Info.vue";
-  import Server from "../components/Card/Server.vue";
-  import {isLarge} from '../plugins/common'
-  const arr = ref([])
+  import {GetLogs} from "../plugins/axios";
+  import Console from "../components/Element/Console.vue";
+  const page = ref(1)
+  const logs = ref([])
+  const getLogs = async () => {
+    await GetLogs(page.value, 100).then(res => {
+      logs.value = res.data
+    })
+  }
+  getLogs()
+  const next = (first) => {
+    first === 1 ? page.value = 1 : page.value++
+    getLogs()
+  }
 </script>
