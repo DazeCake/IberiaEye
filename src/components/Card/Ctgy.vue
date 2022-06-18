1c6e978e9ae09690581c7226fd7bb3aba6ca94a5
skadiD <kazdel@qq.com>
2022-06-18 22:28:21 +0800

    devices page

--- src/components/Card/Ctgy.vue
+++ src/components/Card/Ctgy.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-    <img class="rounded-t-lg" :src=" + data['Bg'] + '.png'" alt="" />
     <div class="p-5">
-      <h5 class="mb-2 text-2xl font-bold text-white">{{data['Title']}}</h5>
-      <p class="mb-3 font-normal text-gray-300">
-        设备名&emsp;：8848钛金手机<br>
-        添加时间：昨天
-      </p>
+      <h5 class="mb-2 text-2xl font-bold text-white flex items-center justify-between">
+        #{{data['id'] + ' ' +data['deviceName']}}
+        <div class="badge text-green-500 badge-outline badge-lg">{{ status(data['status']) }}</div>
+      </h5>
+      <p class="mb-3 font-normal text-gray-300"><span class="text-orange-500">设备Token：</span><br>{{ data['deviceToken'] }}</p>
+      <p class="mb-3 font-normal text-gray-300"><span class="text-orange-500">到期时间：</span><br>{{ data['expireTime'] }}</p>
       <button class="btn btn-info btn-block">设备设置 →</button>
     </div>
   </div>
@@ -15,4 +15,18 @@
   defineProps({
     data: Object
   })
+  const status = (tag) => {
+    switch (tag) {
+      case '0':
+        return '离线'
+      case '1':
+        return '空闲'
+      case '1001':
+        return '日常'
+      case '1002':
+        return '肉鸽'
+      default:
+        return '未知'
+    }
+  }
 </script>
